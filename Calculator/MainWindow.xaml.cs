3c0a6cf4ba8c5a14bd443df772141e12706c1f7f
alan-h-morrison <amorrison12312@gmail.com>
2021-02-25 17:08:13 +0000

    Button correspond to content

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,45 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double value = 0;
+        string operation = "";
+        bool operationPressed = false;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Button b = (Button)sender;
+            string num = (string)b.Content;
+
+            if ((txtAnswer.Text == "0" && num != "." && num != "0") || (operationPressed))
+            {
+                txtAnswer.Clear();
+            }
+
+            if(num != "0")
+            {
+                txtAnswer.Text = txtAnswer.Text + b.Content;
+            }
+        }
+
+        private void btnCLearEntry_Click(object sender, RoutedEventArgs e)
+        {
+            txtAnswer.Text = "0";
+        }
+
+        private void operator_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            operation = (string)b.Content;
+            value = Double.Parse(txtAnswer.Text);
+            operationPressed = true;
+
+            
         }
     }
 }
